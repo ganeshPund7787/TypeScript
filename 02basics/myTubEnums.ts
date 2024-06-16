9afb695f52c8ea504218411a45c26f1dc3fb9764
Ganesh Pund <ganupund7787@gmail.com>
2024-06-16 14:59:10 -0700

    tuples in ts

---
+++ 02basics/myTubEnums.ts
@@ -0,0 +1,13 @@
+const user: (number | string)[] = [1, "gp"];
+
+let tuser: [number, string, boolean];
+
+tuser = [1, "ganu", true]; // only allowodin serial wise
+
+const rgb: [number, number, number] = [255, 123, 231];
+
+type User = [number, string];
+
+const newUser: User = [23, "Ganu"];
+
+newUser[1] = "Ganesh";
