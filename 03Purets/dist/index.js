5891fbaf39d5c7d218952268807efcb88cc00a8c
Ganesh Pund <ganupund7787@gmail.com>
2024-06-16 18:31:40 -0700

    Protected access modifire

--- 03Purets/dist/index.js
+++ 03Purets/dist/index.js
@@ -11,11 +11,32 @@ class User {
 class User2 {
     email;
     name;
+    _courseCount = 1;
     city = "shevgaon";
-    constructor(email, name // private userId: string
-    ) {
+    constructor(email, name) {
         this.email = email;
         this.name = name;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple ${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count must be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class subUser extends User2 {
+    isFamily = true;
+    changeCourseCount() {
+        this._courseCount = 4;
     }
 }
 const ganesh = new User("ganu@gmail.com", "ganesh");
--- 03Purets/src/index.ts
+++ 03Purets/src/index.ts
@@ -9,11 +9,36 @@ class User {
 }
 
 class User2 {
+  protected _courseCount = 1;
   readonly city: string = "shevgaon";
-  constructor(
-    public email: string,
-    public name: string // private userId: string
-  ) {}
+
+  constructor(public email: string, public name: string) {}
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  get getAppleEmail(): string {
+    return `apple ${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count must be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
+}
+
+class subUser extends User2 {
+  isFamily: boolean = true;
+  changeCourseCount() {
+    this._courseCount = 4;
+  }
 }
 
 const ganesh = new User("ganu@gmail.com", "ganesh");
