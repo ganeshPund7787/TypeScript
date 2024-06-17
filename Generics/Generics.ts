b9002236cf2b76d1a8d0bba4f791dfaf4694ab50
Ganesh Pund <ganupund7787@gmail.com>
2024-06-17 07:40:16 -0700

    detefction & type guards

--- Generics/Generics.ts
+++ Generics/Generics.ts
@@ -32,8 +32,44 @@ function getSearchProducts<T>(product: T[]): T {
   return product[myIdx];
 }
 
-const getMoreSearchProducts = <T,>(product: T[]): T => {
+const getMoreSearchProducts = <T>(product: T[]): T => {
   // do some databse operation
   const myIdx = 3;
   return product[myIdx];
 };
+
+// Advanced generics
+
+interface database {
+  connection: string;
+  username: string;
+  password: string;
+}
+
+function anotherFunctio<T, U extends database>(valOne: T, valTwo: U): Object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+
+// anotherFunctio(23, {});
+
+interface Quize {
+  name: string;
+  type: string;
+}
+
+interface Course {
+  name: string;
+  author: string;
+  duration: string;
+}
+
+class syllabus<T> {
+  public cart: T[] = [];
+
+  addCard(product: T) {
+    this.cart.push(product);
+  }
+}
