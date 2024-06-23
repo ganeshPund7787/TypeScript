5e0b9c6faba2ae61338f4afd19e719c7cb0c9db4
Ganesh Pund <ganupund7787@gmail.com>
2024-06-23 10:37:05 -0700

    test frontend

---
+++ fontend/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default App;
